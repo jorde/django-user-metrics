58e4db886e6e399068e58fa1aaced9b5172e0e3e
Jori Lallo <jtlallo@gmail.com>
2012-01-24 00:54:00 -0800

    Show daily and weekly stats in admin

--- user_metrics/admin.py
+++ user_metrics/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
-from user_metrics.models import Metric
+from user_metrics.models import Metric, MetricDay, MetricWeek
+
+class MetricDayAdmin(admin.ModelAdmin):
+    list_display = ('date_up', 'metric', 'user', 'count', )
+    ordering = ('date_up',)
+    search_fields = ('date_up',)
+
+class MetricWeekAdmin(admin.ModelAdmin):
+    list_display = ('date_up', 'metric', 'user', 'count', )
+    ordering = ('date_up',)
+    search_fields = ('date_up',)
 
 admin.site.register(Metric)
+admin.site.register(MetricDay, MetricDayAdmin)
+admin.site.register(MetricWeek, MetricWeekAdmin)
